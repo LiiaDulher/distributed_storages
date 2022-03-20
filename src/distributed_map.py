070c8ef8caa32d9cf72f0b5907c6cf321d19ed71
Liia Dulher <dulher@ucu.edu.ua>
2022-03-20 13:47:16 +0200

    map class

---
+++ src/distributed_map.py
@@ -0,0 +1,26 @@
+import hazelcast
+
+
+class DistributedMap:
+
+    def __init__(self):
+        hz = hazelcast.HazelcastClient()
+        self.map = hz.get_map("DistributedMap").blocking()
+
+    def __getitem__(self, key):
+        return self.map.get(key)
+
+    def __setitem__(self, key, value):
+        self.map.put(key, value)
+
+    def __delitem__(self, key):
+        self.map.delete(key)
+
+    def __contains__(self, key):
+        return self.map.contains_key(key)
+
+    def __len__(self):
+        return self.map.size()
+
+    def __repr__(self):
+        return self.map.values()
